f99729ddc705e608a0c02057d2bb0c5088be87c3
Arnav Reddy <arnavreddy@gmail.com>
2020-07-23 16:21:54 -0700

    Init proj with shared preferences onboard checking

---
+++ Legato/lib/auth/auth.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  @override
+  _Auth createState() => _Auth();
+}
+
+class _Auth extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Auth"),
+      ),
+    );
+  }
+}
---
+++ Legato/lib/home/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("home"),
+      ),
+    );
+  }
+}
